530852c5a9c7bd59f469e69ec0ceab0d877c372f
MiltonLai <millton.lai@gmail.com>
2020-04-29 15:32:27 +0800

    Remove useless files; update ojdbc14 to ojdbc6

--- src/main/java/com/rockbb/robatis/service/inf/BasicService.java
+++
@@ -1,8 +0,0 @@
-package com.rockbb.robatis.service.inf;
-
-public interface BasicService
-{
-	// Use this instead of Integer.MAX_VALUE because it will be added "1" as ROWNUM in Oracle DB, which will cause trouble
-	int INT_MAX = Integer.MAX_VALUE - 1;
-	String[] ORDER = {"DESC", "ASC"};
-}
